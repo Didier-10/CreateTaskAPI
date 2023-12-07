ef61c88aa3bc8fd2529172e7c486477a15f8159e
Didier Páez López <dm.pl1920@gmail.com>
2023-12-06 20:18:12 -0600

    ADD CT002 the schemas are added

---
+++ schemas/create_task_schemas.py
@@ -0,0 +1,23 @@
+from marshmallow import fields, validates, ValidationError
+
+class CreateTaskSchema:
+    title = fields.String(required=True)
+    description = fields.String(required=False)
+    color = fields.String(required=True)
+
+    @validates('title')
+    def validate_title(self, value):
+        if len(value) < 120:
+            raise ValidationError('Title must be at leat 120 characters long.')
+        
+    @validates('description')
+    def validate_author(self, value):
+        if len(value) < 300:
+            raise ValidationError('Description must be at leat 300 characters long.')
+    
+    @validates('color')
+    def validate_color(self, value):
+        if len(value) < 7:
+            raise ValidationError('Color must be at leat 7 characters long.')
+        
+    
